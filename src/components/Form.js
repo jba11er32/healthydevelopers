2cddca6f1b9885658b1752ede94c668be30e8fc4
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-07-02 02:59:42 -0700

    Rename Form.js to HabitForm.js and created habit form page

--- src/components/Form.js
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-import { API_URL } from '../config';
---
+++ src/components/HabitForm.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { API_URL } from '../config';
+
+const HabitForm = () => {
+	const dailyInput = {
+		water: '',
+		pushups: '',
+		situps: '',
+		squats: '',
+	};
+
+	const [dailyData, setDailyData] = useState(dailyInput);
+
+	const handleChange = (event) => {
+		setDailyData({ ...dailyData, [event.target.id]: event.target.value });
+	};
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		const url = `${API_URL}/habits/create`;
+		fetch(url, {
+			method: 'POST',
+			body: JSON.stringify(dailyData),
+			headers: {
+				Authorization: `Bearer ${window.localStorage.getItem('token')}`,
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((response) => response.json())
+			.then((result) => {
+				console.log('Success:', result);
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='water'>Water</label>
+				<input
+					type='text'
+					value={dailyData.water}
+					onChange={handleChange}
+					placeholder='x cups'
+				/>
+
+				<label htmlFor='pushups'>Push-Ups</label>
+				<input
+					type='text'
+					value={dailyData.pushups}
+					onChange={handleChange}
+					placeholder='0'
+				/>
+
+				<label htmlFor='situps'>Sit-Ups</label>
+				<input
+					type='text'
+					value={dailyData.situps}
+					onChange={handleChange}
+					placeholder='0'
+				/>
+
+				<label htmlFor='squats'>Squats</label>
+				<input
+					type='text'
+					value={dailyData.squats}
+					onChange={handleChange}
+					placeholder='0'
+				/>
+
+				<br />
+				<button type='submit' value='submit' />
+			</form>
+		</div>
+	);
+};
+
+export default HabitForm;
