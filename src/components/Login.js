659764b038b6f303150e627756dadfa4f5daf2e6
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-07-03 16:49:07 -0700

    Update Login.js with password min and max length

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,92 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { API_URL } from '../config';
 
 const Login = () => {
-	const [userName, setUserName] = useState('');
-	const [password, setPassword] = useState('');
+	const pwdMaxLength = 20;
+	const pwdMinLength = 8;
+
+	const [state, setState] = useState({
+		email: '',
+		password: '',
+	});
 
 	function handleSubmit(event) {
 		event.preventDefault();
 		const userData = {
-			email: userName,
-			password: password,
+			"email": state.email,
+			"password": state.password
 		};
+
+		const url = `${API_URL}/users/login`;
+
+		fetch(url, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(userData),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				window.localStorage.setItem('token', data.token);
+				window.localStorage.setItem('userId', data.foundUser._id);
+			})
+			.then(
+				setTimeout(() => {
+					window.location.reload(true);
+				}, 150)
+			)
+			.catch((data) => {});
 	}
 
-	return <div></div>;
+	function handleChange(event) {
+		const value = event.target.value;
+		setState({
+			...state,
+			[event.target.name]: value,
+		});
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label>
+					Email
+					<input
+						type='text'
+						name='email'
+						value={state.email}
+						onChange={handleChange}
+						placeholder='someone@someone.com'
+					/>
+				</label>
+
+				<label>
+					Password
+					<input
+						type='password'
+						name='password'
+						value={state.password}
+						onChange={handleChange}
+						minLength={pwdMinLength}
+						maxLength={pwdMaxLength}
+						placeholder='8-20 characters'
+					/>
+				</label>
+
+				<div>
+					<button type='submit'>Log In</button>
+				</div>
+
+				<div className='divide-line'></div>
+
+				<div>
+					<Link to='/signup'>
+						<button type='button'>Sign Up</button>
+					</Link>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default Login;
---
+++ src/components/LoginButtons.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoginButtons = () => {
+	return <div></div>;
+};
+
+export default LoginButtons;
